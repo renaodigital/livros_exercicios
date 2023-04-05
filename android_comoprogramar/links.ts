8341508e2a5ff162757347ef3bd67e224bbf1ad8
Renão <78944713+renaodigital@users.noreply.github.com>
2023-04-05 11:28:01 -0300

    05/04/2023 [11h27min]

--- android_comoprogramar/links.ts
+++ android_comoprogramar/links.ts
@@ -1,34 +1,32 @@
-<p ng-repeat="site in siteList">Visit <a href="site.url">{{site.name}}</a></p>
+<p ng-repeat="site in siteList">Page 10 Content:<a href="site.url">{{site.name}}</a></p>
 
 this.siteList= [
-    { name: "Google", url: "http://www.google.com", comment: page + content},
-    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
+    { name: "Deitel: Book Updates", url: "https://deitel.com/books/AndroidHTP2", comment: page10 + book updates by the publisher},
+    { name: "XML Updt1", url: "http://www.deitel.com/articles/xml_tutorials/20060401/XMLBasics", comment: page10 + basic principles envolving XML language to learn about Java },
+    { name: "XML Updt2", url: "http://www.deitel.com/articles/xml_tutorials/20060401/XMLStructuringData/", comment: page10 + how to structure some XML content },
+    { name: "XML Updt3", url: "http://ibm.com/developerworks/xml/newto", comment: page10 + xml for dummies (rules and basics for new adventurers) },
+    { name: "XML Updt4", url: "http://w3schools.com/xml_whatis.asp", comment: page10 + community content exposing what xml is }
 ];
+
+<p ng-repeat="site in siteList">Page 11 Content:<a href="site.url">{{site.name}}</a></p>
 
 this.siteList= [
-    { name: "Google", url: "http://www.google.com", comment: page + content},
-    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
+    { name: "Android Good Practices", url: "http://developer.android.com/guide/practices/index.html", comment: page11 + good practices for android developers, always keep update about it and the market }
 ];
+
+<p ng-repeat="site in siteList">Page 13 Content:<a href="site.url">{{site.name}}</a></p>
 
 this.siteList= [
-    { name: "Google", url: "http://www.google.com", comment: page + content},
-    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
+    { name: "Source Code", url: "https://ww.grupoa.com.br/", comment: page13 + source code from the all the book content }
 ];
+
+<p ng-repeat="site in siteList">Page 14 Content:<a href="site.url">{{site.name}}</a></p>
 
 this.siteList= [
-    { name: "Google", url: "http://www.google.com", comment: page + content},
-    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
+    { name: "Developer Documentation: Android", url: "http://developer.android.com/", comment: page13 + source code from the all the book content },
+    { name: "Developer Documentation: Java", url: "http://oracle.com/technetwork/java/javase/downloads.index.html", comment: page14 + source code from the all the book content },
+    { name: "Developer Documentation: Eclipse (Android Utility)", url: "http://oracle.com/technetwork/java/javase/downloads.index.html", comment: page14 + source code from the all the book content },
+    { name: "Android Studio: Download", url: "http://developer.android.com/sdk/installing/studio.html", comment: page14 + source code from the all the book content },
 ];
-
-this.siteList= [
-    { name: "Google", url: "http://www.google.com", comment: page + content},
-    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
-];
-
-this.siteList= [
-    { name: "Google", url: "http://www.google.com", comment: page + content},
-    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
-];
-
 
 

32030e08e2e9430c908300d106b956b8b870609d
Renão <78944713+renaodigital@users.noreply.github.com>
2023-04-05 11:00:25 -0300

    05/04/2023 [11h00min]

---
+++ android_comoprogramar/links.ts
@@ -0,0 +1,8 @@
+<p ng-repeat="site in siteList">Visit <a href="site.url">{{site.name}}</a></p>
+
+this.siteList= [
+    { name: "Google", url: "http://www.google.com", comment: page + content},
+    { name: "StackOverflow", url: "http://www.stackoverflow.com", comment: page + content }
+];
+
+

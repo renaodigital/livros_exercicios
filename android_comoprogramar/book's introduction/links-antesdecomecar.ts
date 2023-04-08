6af8bdadc28df32e70710277858acf6edea4f5cf
Renão <78944713+renaodigital@users.noreply.github.com>
2023-04-08 07:01:30 -0300

    08/04/2023 (travelling_updt) [07h01min]

---
+++ android_comoprogramar/book's introduction/links-antesdecomecar.ts
@@ -0,0 +1,16 @@
+<p ng-repeat="site in siteList">Page 19 Content:<a href="site.url">{{site.name}}</a></p>
+
+this.siteList= [
+    { name: "Livro completo: suporte completo no website", url: "https://www.deitel.com/books/AndroidHTP2", comment: page19 + book updates by the publisher },
+    { name: "Download da SDK", url: "http://develoeper.android.com/sdk/index.html", comment: page19 + basic requirement to develop an android app  },
+    { name: "Download da JDK", url: "http://oracle.com/technetwork/java/javase/downloads/index.html", comment: page19 + how to structure some XML content },
+    { name: "Instruções de instalação da JDK", url: "http://docs.oracle.com/javase/7/docs/webnotes/install/index.html", comment: page19 + how to download JDK tools on x32 and x64 operational system }
+];
+
+<p ng-repeat="site in siteList">Page 20 Content:<a href="site.url">{{site.name}}</a></p>
+
+this.siteList= [
+    { name: "Download Android SDK/ADT Bundle", url: "http://deitel.com/books/AndroidHTP2", comment: page20 + download android to develop some tools or increments },
+    { name: "Opções para usar o Eclipse", url: "https://www.grupoa.com.br", comment: page20 + options to install android sdk/adt bundle in portuguese },
+    { name: "Instalando o Android Studio", url: "http://developer.android.com/sdk/installing/studio.html", comment: page20 + installing android studio on computer },
+];

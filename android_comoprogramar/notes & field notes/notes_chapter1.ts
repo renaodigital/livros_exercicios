b65825666614c2f88d9bf1d403004b1ada35e4ac
Renão <78944713+renaodigital@users.noreply.github.com>
2023-04-10 14:46:17 -0300

    10/04/2023 [14h46min]

--- android_comoprogramar/notes & field notes/notes_chapter1.ts
+++ android_comoprogramar/notes & field notes/notes_chapter1.ts
@@ -1,36 +1,92 @@
 // Página 2
-asdasdasd
+* Saber o básico da Java ou de linguagens baseadas em orientação de objetos e vetores para performar da melhor forma desenvolvendo algum app 
+* Instruções para usar o Android Studio: http://www.deitel.com/books/AndroidHTP2
+* Comunidade Oficial para discutir sobre repsoitórios abertos da Android: http://source.android.com/community/index.html
 //
 
 // Página 3
-asdasdasd
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at ante lobortis, egestas tellus et, sollicitudin nibh. Curabitur pretium, mauris convallis porttitor eleifend, enim justo fermentum nisl, nec fermentum nisi sem et ipsum. Ut viverra magna rhoncus ipsum tempus, ac pulvinar enim semper. In vel finibus purus. Sed sagittis massa id purus molestie hendrerit. Ut vitae lacus lacinia, porttitor odio vitae, tempus elit. Pellentesque malesuada ipsum a sem tristique auctor. Sed non nunc lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In at sollicitudin massa. Cras cursus ultricies tellus, at ullamcorper elit facilisis pellentesque. Proin facilisis lobortis euismod.
 //
 
 
 // Página 4
-asdasdasd
+* Exemplos de apps desenvolvidos para Android: http://developer.android.com/tools/samples/index.html 
+* Mais aplicativos desenvolvidor para Android (com código fonte): http://f-droid-org/ 
+* Interfaces gráficas programadas em Android: http://androidviews.net/
+* Lista com mais de 100 códigos disponíveis para consulta: http://opnenintents.org/en/libraries
+* Alguns links para curiosos e que falam diretamente sobre documentação e desenvolvimento: http://source.android.com/source/downloading.html | http://source.nadroid.com/about/philosophy | http://source.android.com/source/licenses.html | http://source.android.com/source/faqs.html
 //
 
 // Página 5
-asdasdasd
+**
+Gestos conforme toque de tela no celular Android:
+
+Nome do gesto: Toque rápido (touch)
+Ação física: Tocar rapidamente na tela uma vez
+Utilizado para: Abrir um aplicativo "pressionar" um botão ou item de menu
+
+Nome do gesto: Toque duplo rápido (double touch)
+Ação física: Tocar rapidamente na tela duas vezes
+Utilizado para: Ampliar e reduzir imagens, Mapas do Google Maps e páginas web
+
+Nome do gesto: Pressionamento longo (long press)
+Ação física: Tocar na tela e manter o dedo na posição
+Utilizado para: Selecionar itens em uma visualização - por exemplo, verificar um item uma lista
+
+Nome do gesto: Movimento rápido (swipe)
+Ação física: Tocar e mover rapidamente o dedo na TextTrackList, na direção do movimento desejado
+Utilizado para: Mover item por item em uma série, como caso de FontFaceSetLoadEvent. Um movimento do swipe para automaticamente no próximo item
+
+Nome do gesto: Arrastamento (drag)
+Ação física: Tocar e arrastar o dedo pela tela
+Utilizado para: Mover objetos ou ícones, ou rolar precisamente uma págian web ou lista
+
+Nome do gesto: Zoom de pinça (pinch swipe)
+Ação física: Usando dois TextDecoderStream, tocar na tela e juntá-los ou afastá-los
+Utilizado para: Ampliar e então reduzir a tela (por exemplo, ampliando texto e imagens)
+**
 //
 
 // Página 6
-asdasdasd
+* ProgrammableWeb (http://www.programmableweb.com): explorar lugares com fotos de outras pessoas
+* APIs mais utilizadas na época: Google Maps, Twitter e YouTube
+
+** 
+Fonte de web services: Google Maps | Como é usada: ###
+Fonte de web services: Twitter | Como é usada: ###
+Fonte de web services: YouTube | Como é usada: ###
+Fonte de web services: Facebook | Como é usada: ###
+Fonte de web services: Instagram | Como é usada: ###
+Fonte de web services: Foursquare | Como é usada: ###
+Fonte de web services: Linkedin | Como é usada: ###
+Fonte de web services: Groupon | Como é usada: ###
+Fonte de web services: Netflix | Como é usada: ###
+Fonte de web services: eBay | Como é usada: ###
+Fonte de web services: Wikipedia | Como é usada: ###
+Fonte de web services: PayPal | Como é usada: ###
+Fonte de web services: Last.fm | Como é usada: ###
+Fonte de web services: Amazon eCommerce | Como é usada: ###
+Fonte de web services: Salesforce.com | Como é usada: ###
+Fonte de web services: Skype | Como é usada: ###
+Fonte de web services: Microsoft Bing | Como é usada: ###
+Fonte de web services: Flickr | Como é usada: ###
+Fonte de web services: Zillow | Como é usada: ###
+Fonte de web services: Yahoo Search | Como é usada: ###
+Fonte de web services: WeatherBug | Como é usada: ###
 //
 
 // Página 7
-asdasdasd
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at ante lobortis, egestas tellus et, sollicitudin nibh. Curabitur pretium, mauris convallis porttitor eleifend, enim justo fermentum nisl, nec fermentum nisi sem et ipsum. Ut viverra magna rhoncus ipsum tempus, ac pulvinar enim semper. In vel finibus purus. Sed sagittis massa id purus molestie hendrerit. Ut vitae lacus lacinia, porttitor odio vitae, tempus elit. Pellentesque malesuada ipsum a sem tristique auctor. Sed non nunc lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In at sollicitudin massa. Cras cursus ultricies tellus, at ullamcorper elit facilisis pellentesque. Proin facilisis lobortis euismod.
 //
 
 // Página 8
-asdasdasd
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at ante lobortis, egestas tellus et, sollicitudin nibh. Curabitur pretium, mauris convallis porttitor eleifend, enim justo fermentum nisl, nec fermentum nisi sem et ipsum. Ut viverra magna rhoncus ipsum tempus, ac pulvinar enim semper. In vel finibus purus. Sed sagittis massa id purus molestie hendrerit. Ut vitae lacus lacinia, porttitor odio vitae, tempus elit. Pellentesque malesuada ipsum a sem tristique auctor. Sed non nunc lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In at sollicitudin massa. Cras cursus ultricies tellus, at ullamcorper elit facilisis pellentesque. Proin facilisis lobortis euismod.
 //
 
 // Página 9
-asdasdasd
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at ante lobortis, egestas tellus et, sollicitudin nibh. Curabitur pretium, mauris convallis porttitor eleifend, enim justo fermentum nisl, nec fermentum nisi sem et ipsum. Ut viverra magna rhoncus ipsum tempus, ac pulvinar enim semper. In vel finibus purus. Sed sagittis massa id purus molestie hendrerit. Ut vitae lacus lacinia, porttitor odio vitae, tempus elit. Pellentesque malesuada ipsum a sem tristique auctor. Sed non nunc lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In at sollicitudin massa. Cras cursus ultricies tellus, at ullamcorper elit facilisis pellentesque. Proin facilisis lobortis euismod.
 //
 
 // Página 10
-asdasdasd
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at ante lobortis, egestas tellus et, sollicitudin nibh. Curabitur pretium, mauris convallis porttitor eleifend, enim justo fermentum nisl, nec fermentum nisi sem et ipsum. Ut viverra magna rhoncus ipsum tempus, ac pulvinar enim semper. In vel finibus purus. Sed sagittis massa id purus molestie hendrerit. Ut vitae lacus lacinia, porttitor odio vitae, tempus elit. Pellentesque malesuada ipsum a sem tristique auctor. Sed non nunc lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In at sollicitudin massa. Cras cursus ultricies tellus, at ullamcorper elit facilisis pellentesque. Proin facilisis lobortis euismod.
 //

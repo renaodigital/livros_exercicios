7801a0403e099a6e4013d12bed2f2f39224889d2
Renão <78944713+renaodigital@users.noreply.github.com>
2023-04-15 03:40:23 -0300

    15/04/2023 [03h40min]

--- fast_notes.ts
+++ fast_notes.ts
@@ -9,3 +9,29 @@ android documentation notes {
 }
 
 
+links_material_ui (
+        newsletter: https://services.google.com/fb/forms/materialdesignnewsletter/
+        utilties:
+        https://m2.material.io/
+        https://m2.material.io/whats-new
+        https://m2.material.io/resources
+        https://m3.material.io/components
+        https://m2.material.io/design/interaction/states.html
+        https://m2.material.io/design/iconography/product-icons.html#design-principles
+        https://developer.android.com/guide/practices/screens_support
+        https://m2.material.io/design/iconography/product-icons.html#design-principles
+        https://m2.material.io/design/interaction/states.html#usage
+        https://m2.material.io/design/environment/surfaces.html#material-environment
+        https://m2.material.io/design/color/applying-color-to-ui.html
+        https://m2.material.io/design/typography/understanding-typography.html#type-properties
+        https://m2.material.io/design/iconography/system-icons.html#design-principles
+        https://m2.material.io/components/buttons#text-button
+        https://m2.material.io/components/buttons-floating-action-button
+        https://m2.material.io/design/shape/shape-as-expression.html#communicating-state
+        https://m2.material.io/design/material-studies/about-our-material-studies.html
+        https://m2.material.io/design/color/the-color-system.html
+
+      
+
+)
+
